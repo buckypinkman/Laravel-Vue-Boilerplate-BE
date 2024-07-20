73e593519fd87d23af2a38059fd7f9564a6c30e6
Rangga Dimas <rangga7765@gmail.com>
2024-07-20 21:46:21 +0700

    cleaning unused feature

--- app/Enums/RolesEnum.php
+++ app/Enums/RolesEnum.php
@@ -5,11 +5,5 @@ namespace App\Enums;
 enum RolesEnum: string {
     case SUPER_ADMIN = 'super admin';
     case ADMIN = 'admin';
-    case AGEN = 'agen';
-    case KADIS = 'kadis';
-    case KABAG = 'kabag';
-    case KASI = 'kasi';
-    case KARAN = 'karan';
-    case KAUR = 'kaur';
-    case MEMBER = 'member';
+    case ECOM_ADMIN = 'ecommerce_admin';
 }
